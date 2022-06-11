5995b402de8ee5c6c827974afcb815f55e5c8f12
unknown <jhonnatha16011987@yahoo.com.br>
2022-06-10 21:01:41 -0300

    Habilitando o cors para login

--- src/main/java/com/jhonnatha/sgc/security/SecurityConfig.java
+++ src/main/java/com/jhonnatha/sgc/security/SecurityConfig.java
@@ -43,11 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/**"
 	};
 
-	private static final String[] PUBLIC_MATCHERS_POST = { "/login" };
+	private static final String[] PUBLIC_MATCHERS_POST = { "/login'" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		
+		http.cors().and().authorizeRequests().antMatchers("/login").permitAll();
 		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(HttpMethod.OPTIONS, PUBLIC_MATCHERS_OPTIONS).permitAll()
